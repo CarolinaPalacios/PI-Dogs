9ff83b6ce39412896e19012309f9df1cd0bde4e3
Carolina Palacios <caaro.palacios@hotmail.com>
2023-07-17 19:25:39 -0300

    renderizado condicional

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 import Loading from "../Loading/Loading";
 import { resetState, getDogs, getTemperaments } from "../../redux/actions";
@@ -7,6 +7,7 @@ import { useDispatch } from "react-redux";
 import style from "./NavBar.module.css";
 
 const NavBar = () => {
+  const location = useLocation();
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(true);
   const [isClicked, setIsClicked] = useState(false);
@@ -34,10 +35,12 @@ const NavBar = () => {
       <NavLink to="/home" className={style.link}>
         <button className={style.homeButton}>HOME</button>
       </NavLink>
-      <SearchBar />
-      <NavLink to="/create" className={style.link}>
-        <button className={style.createButton}>CREATE YOUR DOG</button>
-      </NavLink>
+      {location.pathname === "/home" && <SearchBar />}
+      {location.pathname !== "/create" && (
+        <NavLink to="/create" className={style.link}>
+          <button className={style.createButton}>CREATE YOUR DOG</button>
+        </NavLink>
+      )}
     </div>
   );
 };
